abb21b87d0ecf9352a96ed63fb01a1b3c24ecd76
ISC <insu4149@gmail.com>
2019-12-10 17:31:19 +0900

    Update DcinsideR.R

--- Rcode/DcinsideR.R
+++ Rcode/DcinsideR.R
@@ -12,26 +12,26 @@ library(rvest)
 library(dplyr)
 
 
-### 
+### 셀레니움 연결
 remDr <- remoteDriver(remoteServerAddr = 'localhost', 
                       port = 4445L, #
                       browserName = "chrome") 
 remDr$open() 
 
-### 
+### 받을 경로 지정 벡터 초기화
 setwd("c:/R_data/Dcinside")
 all.title<- c()
 all.rank <- c()
 all.list.dc = list()
 
-####
+#### URL 설정및 소스 가져오기
 
   url = 'https://www.dcinside.com/'
   
   remDr$navigate(url)
-  
   page_source = remDr$getPageSource()[[1]]
   
+##랭킹페이지 얻어오기
   rank.name <-read_html(page_source) %>%
     html_nodes(xpath='//*[@id="rank_gall"]/ol[6]/li[*]/a/span[2]')%>%
     html_text()
@@ -42,7 +42,7 @@ all.list.dc = list()
    
   
   rank.df = data.frame(rank.name,rank.url)
-####
+#### 얻어온 페이지를 이용해 1~10위에 있는 갤러리 탐색
   max=1;
 for(rnk in 1:10){
   dc.title<- c()
@@ -57,9 +57,6 @@ for(rnk in 1:10){
     title <-read_html(page_source) %>%
       html_nodes(xpath='//*[@id="container"]/section[1]/article[2]/div[2]/table/tbody/tr[*]/td[2]/a[1]/text()')%>%
       html_text()
-    
-  
-    
     dc.title <- c(dc.title, title)
   }
   dc.title = dc.title[1:(max*100)]
@@ -67,12 +64,11 @@ for(rnk in 1:10){
     all.list.dc = c(all.list.dc,list.dc)
   
 }
-  dc.title
-  #
+ 
+  
+  ##csv 파일로 저장
   names(all.list.dc) = rank.df[[1]]
-  #########################
   df.dcinside = data.frame(all.list.dc)
-  ###################################
   filestr = paste(Sys.time(),".csv")
   filestr = gsub(pattern = ":", 
       replacement = "",
